87d3aeed362aed3c9ecaac96619948a935b78900
Jia Chang <smile001108@163.com>
2022-02-03 16:15:14 +0800

    complete username password input errorMsg show

--- src/views/login/components/LoginModule.vue
+++ src/views/login/components/LoginModule.vue
@@ -1,18 +1,77 @@
 <template>
   <div id="loginContent">
-        <input class="username" type="text" placeholder="学号">
-        <input class="password" type="password" placeholder="密码">
-        <button class="loginBtn">登录</button>
+        <input class="username" type="text" :placeholder="usernameHolder" v-model="username" ref="username" @focus="inputFocus('username')">
+        <div class="errorMsg" ref="usernameError">{{usernameErrorMsg}}</div>
+        <input class="password" type="password" :placeholder="passwordHolder" v-model="password" ref="password" @focus="inputFocus('password')">
+        <div class="errorMsg" ref="passwordError">{{passwordErrorMsg}}</div>
+        <button class="loginBtn" @click="login()">登录</button>
   </div>
 </template>
 
 <script>
     export default {
-        name: 'LoginModule'
+        name: 'LoginModule',
+        components: {
+
+        },
+        data() {
+            return {
+                username: '',
+                password: '',
+                usernameHolder: '学号',
+                passwordHolder: '密码',
+                usernameErrorMsg: '学号长度不能超过20',
+                passwordErrorMsg: '密码长度不能超过20',
+            }
+        },
+        methods: {
+            // input框获取焦点之后 将errorMsg 不可见 并且颜色恢复
+            inputFocus(dom) {
+                let usernameDom = this.$refs.username;
+                let passwordDom = this.$refs.password;
+                let usernameErrorDom = this.$refs.usernameError;
+                let passwordErrorDom = this.$refs.passwordError;
+                if(dom === 'username') {
+                    usernameDom.style.setProperty('border-bottom', '1px solid #ebebeb')
+                    usernameErrorDom.style.setProperty('visibility', 'hidden')
+                } else {
+                    passwordDom.style.setProperty('border-bottom', '1px solid #ebebeb')
+                    passwordErrorDom.style.setProperty('visibility', 'hidden')
+                }
+            },
+            login() {
+                let username = this.username;
+                let password = this.password;
+                let usernameDom = this.$refs.username;
+                let passwordDom = this.$refs.password;
+                let usernameErrorDom = this.$refs.usernameError;
+                let passwordErrorDom = this.$refs.passwordError;
+                if(username.length > 20) {
+                    usernameDom.style.setProperty('border-bottom', '2px solid red')
+                    this.username = ''
+                    this.usernameErrorMsg = '学号长度不能超过20'
+                    usernameErrorDom.style.setProperty('visibility', 'visible')
+                }
+                if(password.length > 20) {
+                    passwordDom.style.setProperty('border-bottom', '2px solid red');
+                    this.password = ''
+                    this.passwordErrorMsg = '密码长度不能超过20'
+                    passwordErrorDom.style.setProperty('visibility', 'visible')
+                }
+            }
+        }
     }
 </script>
 
 <style>
+
+    .errorMsg {
+        color: red;
+        font-size: 14px;
+        margin-left: 80px;
+        visibility: hidden;
+    }
+
     #loginContent {
         float: left;
         width: 100%;
@@ -24,7 +83,7 @@
         width: 70%;
         height: 30px;
         margin-top: 50px;
-        margin: 50px auto;
+        margin: 40px auto 5px;
         border-top: none;
         border-left: none;
         border-right: none;
