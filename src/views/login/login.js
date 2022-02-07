0c3ba863264686c9c173b6cb1626c8892aa45e6f
Jia Chang <smile001108@163.com>
2022-02-07 12:15:53 +0800

    solve cookie can't save

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import Login from './Login.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+
+axios.defaults.withCredentials=true
 
 new Vue({
   render: h => h(Login),
