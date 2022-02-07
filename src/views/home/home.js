5e6247d0da2af54ce574eb9546d53c908738bac5
Jia Chang <smile001108@163.com>
2022-02-07 17:26:21 +0800

    [front] all page add axios default with credentials

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -4,10 +4,13 @@ import VueRouter from 'vue-router'
 import router from './router'
 import ElementUI from 'element-ui'; // 2.1引入结构
 import 'element-ui/lib/theme-chalk/index.css'; // 2.2引入样式
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(ElementUI)
+
+axios.defaults.withCredentials=true
 
 new Vue({
   render: h => h(Home),
--- src/views/write/write.js
+++ src/views/write/write.js
@@ -4,10 +4,13 @@ import VueRouter from 'vue-router'
 import router from './router'
 import MavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(MavonEditor)
+
+axios.defaults.withCredentials=true
 
 new Vue({
   render: h => h(Write),
