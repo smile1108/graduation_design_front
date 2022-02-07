4ecff330cf81c1b3a877629f702a1b4dbb31dae2
Jia Chang <smile001108@163.com>
2022-02-07 17:24:52 +0800

    [front] complete auto login api and test success

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -7,6 +7,7 @@
 
 <script>
   import MyHeader from './components/MyHeader'
+  import axios from 'axios'
 
   export default {
     name: 'App',
@@ -15,13 +16,28 @@
     },
     data() {
       return {
-        userInfo: {
-
-        }
+        userInfo: {}
       }
     },
-    created() {
-      this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+    methods: {
+      autoTakeUserInfo() {
+        console.log("auto")
+        axios.get("http://localhost:9527/user/autoLogin").then(response => {
+          console.log(response)
+          if(response.data.code == 200) {
+            if(response.data.data === null) {
+              this.userInfo = null
+            } else {
+              this.userInfo = JSON.parse(JSON.stringify(response.data.data))
+            }
+          }
+        })
+      }
+    },
+    mounted() {
+      // 当页面dom渲染好之后 进行自动登录的请求
+      console.log("mounted")
+      this.autoTakeUserInfo()
     }
   }
 </script>
--- src/views/index/main.js
+++ src/views/index/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+
+axios.defaults.withCredentials=true
 
 new Vue({
   render: h => h(App),
