cff90d820969ff432aefa6bc65ca3a306a5cdaca
Jia Chang <smile001108@163.com>
2022-02-15 08:08:22 +0800

    [front] my article list at home page add delete button

--- src/views/home/components/MyArticleFooter.vue
+++ src/views/home/components/MyArticleFooter.vue
@@ -3,6 +3,7 @@
         <div class="classify">计算机类</div>
         <span class="likeContent">10人喜欢</span>
         <span class="publishDate">发布于 2021-2-14 10:00:00</span>
+        <div class="deleteBtn">删除</div>
     </div>
 </template>
 
@@ -34,5 +35,21 @@
     .articleFooter .publishDate {
         margin-left: 50px;
         color: rgb(84, 64, 110);
+    }
+
+    .articleFooter .deleteBtn {
+        display: inline;
+        background-color: #da4f49;
+        border: 1px solid #bd362f;
+        padding: 4px;
+        margin-left: 80px;
+        border-radius: 4px;
+        color: #fff;
+        font-size: 16px;
+    }
+
+    .articleFooter .deleteBtn:hover {
+        cursor: pointer;
+        background-color: #bd362f;
     }
 </style>
