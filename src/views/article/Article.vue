96079b89c48c1415f1001239618b5779ed7491d7
Jia Chang <smile001108@163.com>
2022-03-08 17:15:45 +0800

    [front] call getArticleMessageById api when article component mounted

--- src/views/article/Article.vue
+++ src/views/article/Article.vue
@@ -7,6 +7,7 @@
 <script>
 
     import ArticleMessage from './components/ArticleMessage'
+    import axios from 'axios'
 
     export default {
         name: 'Article',
@@ -15,8 +16,24 @@
         },
         data() {
             return {
-                
+                article: {}
             }
+        },
+        mounted() {
+            // 当页面渲染完成之后 请求获取文章信息的接口
+            let url = "http://localhost:9527/article/getArticleMessageById?articleId=" + this.$route.params.articleId + "&username="
+            let userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+            if(userInfo != null) {
+                url = url + "&username=" + userInfo.username
+            }
+            axios.get(url).then(res => {
+                if(res.data.code === 200) {
+                    // 代表请求成功
+                    this.article = JSON.parse(JSON.stringify(res.data.data))
+                } else {
+                    alert(res.data.msg)
+                }
+            })
         }
     }
 </script>
