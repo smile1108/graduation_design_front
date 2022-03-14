e1381434182c10622ace49d216f332f58285d2e7
Jia Chang <smile001108@163.com>
2022-03-14 11:38:18 +0800

    [front] complete MyQuestion component style

--- src/views/home/components/MyQuestion.vue
+++ src/views/home/components/MyQuestion.vue
@@ -1,15 +1,71 @@
 <template>
     <div id="myQuestion">
-        question
+        <MyQuestionItem v-for="questionObj in myQuestionList" :key="questionObj.id" :questionObj="questionObj"
+        :showPersonalMessage="showPersonalMessage"></MyQuestionItem>
+        <Page :pageMax="pageMax" @changePage="changePage"></Page>
     </div>
 </template>
 
 <script>
+
+    import MyQuestionItem from './MyQuestionItem'
+    import Page from '../../index/components/pageComponent'
+    import axios from 'axios'
+
     export default {
-        name: "MyQuestion"
+        name: "MyQuestion",
+        props: {
+            userInfo: Object,
+            showPersonalMessage: Boolean
+        },
+        components: {
+            MyQuestionItem, Page
+        },
+        data() {
+            return {
+                myQuestionList: [],
+                pageMax: undefined,
+            }
+        },
+        methods: {
+            changePage(page) {
+                let url = "http://localhost:9527/article/question/getUserQuestionList?username=" + this.userInfo.username + "&page=" + (page - 1)
+                axios.get(url).then(res => {
+                    if(res.data.code === 200) {
+                        this.myQuestionList = res.data.data.lists
+                        this.pageMax = res.data.data.sumPage
+                    } else if(res.data.code === 519) {
+                        alert("用户身份认证过期, 请重新登录")
+                        window.location.href = 'login.html'
+                    }
+                })
+            }
+        },
+        mounted() {
+            // 当页面渲染完成之后 调用获取自己问题列表的接口
+            let getUserQuestionListUrl = "http://localhost:9527/article/question/getUserQuestionList?username=" + this.$route.params.username
+            axios.get(getUserQuestionListUrl).then(res => {
+                if(res.data.code === 200) {
+                    this.myQuestionList = res.data.data.lists
+                    this.pageMax = res.data.data.sumPage
+                } else if(res.data.code === 529) {
+                    alert(res.data.msg)
+                    window.location.href = "login.html"
+                } else {
+                    alert(res.data.msg)
+                }
+            })
+        }
     }
 </script>
 
 <style>
-
+    #myQuestion {
+        width: 70%;
+        margin: 10px auto 0;
+        background-color: #fff;
+        margin-top: 0;
+        padding: 30px;
+        box-sizing: border-box;
+    }
 </style>
---
+++ src/views/home/components/MyQuestionItem.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="myQuestionItem">
+        <div class="titleMessage">{{questionObj.title}}</div>
+        <div class="statisticsMessage">
+            <span class="publishDate">发布于{{this.formatDate}}</span>
+            <span class="answerCount">10人回答</span>
+            <div v-if="this.showPersonalMessage" class="deleteQuestionBtn">删除</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyQuestionItem",
+        props: {
+            questionObj: Object,
+            showPersonalMessage: Boolean
+        },
+        computed: {
+            formatDate() { 
+                const arr = this.questionObj.publishDate.split('T');
+                const d = arr[0];
+
+                const t = arr[1];
+                const tarr = t.split('.000');
+                const marr = tarr[0].split(':');
+
+                const dd = d + ' ' + marr[0] + ':' + marr[1] + ':' + marr[2].split('.')[0]
+
+                return dd;
+            }
+        }
+    }
+</script>
+
+<style>
+    #myQuestionItem {
+        width: 70%;
+        padding: 15px;
+        box-sizing: border-box;
+        border-bottom: 1px solid rgb(240, 242, 247);
+    }
+
+    #myQuestionItem .titleMessage {
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    #myQuestionItem .statisticsMessage {
+        margin-top: 10px;
+        font-size: 14px;
+        color: #8590a6;
+    }
+
+    #myQuestionItem .statisticsMessage .answerCount {
+        margin-left: 10px;
+    }
+
+    #myQuestionItem .statisticsMessage .deleteQuestionBtn {
+        display: inline;
+        background-color: #da4f49;
+        border: 1px solid #bd362f;
+        padding: 4px;
+        margin-left: 80px;
+        border-radius: 4px;
+        color: #fff;
+    }
+
+    #myQuestionItem .statisticsMessage .deleteQuestionBtn:hover {
+        cursor: pointer;
+        background-color: #bd362f;
+    }
+</style>
