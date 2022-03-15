94255e28d75e5c291c4ce9d8e75b5e4e7a86626d
Jia Chang <smile001108@163.com>
2022-03-15 09:47:07 +0800

    [front] call getQuestionMessage api when question component created and complete QuestionHeader component data render

--- src/views/question/Question.vue
+++ src/views/question/Question.vue
@@ -1,27 +1,42 @@
 <template>
   <div id="question">
+      <QuestionHeader :question="question" :userInfo="userInfo"></QuestionHeader>
       <QuestionMessage></QuestionMessage>
   </div>
 </template>
 
 <script>
 
+    import QuestionHeader from './components/QuestionHeader'
     import QuestionMessage from './components/QuestionMessage'
+    import axios from 'axios'
 
     export default {
         name: 'Question',
         components: {
-            QuestionMessage
+            QuestionMessage, QuestionHeader
         },
         data() {
             return {
+                question: {},
+                userInfo: {},
             }
         },
         methods: {
             
         },
         created() {
-            
+            this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+            // 当页面渲染完成之后调用获取问题详细信息的接口
+            let getQuestionMessageUrl = "http://localhost:9527/article/question/getQuestionMessageById?questionId=" + this.$route.params.questionId
+            axios.get(getQuestionMessageUrl).then(res => {
+                if(res.data.code === 200) {
+                    this.question = JSON.parse(JSON.stringify(res.data.data))
+                    document.title = this.question.title
+                } else {
+                    alert(res.data.msg)
+                }
+            })
         }
     }
 </script>
---
+++ src/views/question/components/QuestionHeader.vue
@@ -0,0 +1,135 @@
+<template>
+    <div id="articleHeader" class="clearfix">
+        <div class="articleTitleTip">{{question.title}}</div>
+        <div class="writeBtn" @click="jumpWritePage()"><span class="iconfont">&#xe6e2;</span> 提问</div>
+        <div class="userInfo">
+          <a href="login.html" v-if="userInfo === null"><button class="loginBtn" @click="login()">登录</button></a>
+          <div class="userFunction" v-else>
+              <img class="userImg" :src="userInfo.profile" alt="头像">
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "QuestioneHeader",
+        props: {
+            question: Object,
+            userInfo: Object
+        },
+        methods: {
+            jumpWritePage() {
+                if(this.userInfo == null){
+                    alert("请您先登录再进行此操作")
+                    window.location.href = "login.html"
+                } else {
+                    window.location.href = "ask.html"
+                }
+            },
+            login() {
+                window.location.href = "login.html"
+            }
+        }
+    }
+</script>
+
+<style>
+
+    @font-face {
+    font-family: 'iconfont';
+    src: url('../icon/iconfont.woff2?t=1636981770201') format('woff2'),
+        url('../icon/iconfont.woff?t=1636981770201') format('woff'),
+        url('../icon/iconfont.ttf?t=1636981770201') format('truetype');
+    }
+
+    .iconfont {
+        font-family: "iconfont" !important;
+        font-size: 16px;
+        font-style: normal;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        content: '';
+        display: table;
+        clear: both;
+    }
+
+    #articleHeader {
+        width: 100%;
+        height: 60px;
+        line-height: 60px;
+        background-color: #FFF;
+        padding: 0 250px;
+        position: fixed;
+    }
+
+    #articleHeader .articleTitleTip {
+        float: left;
+        font-size: 20px;
+        height: 40px;
+        line-height: 40px;
+        margin-top: 10px;
+        padding: 0 20px;
+        border-left: 2px solid #ebebeb;
+    }
+
+    #articleHeader .writeBtn {
+        font-size: 20px;
+        float: left;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        margin-top: 10px;
+        margin-left: 700px;
+        width: 140px;
+        color: #06f;
+        border: 1px solid #06f;
+        border-radius: 5px;
+    }
+
+    #articleHeader .writeBtn .iconfont {
+        font-size: 24px;
+    }
+
+    #articleHeader .writeBtn:hover {
+        background-color: rgba(0,102,255,.06);
+        cursor: pointer;
+    }
+
+    #articleHeader .userInfo {
+        height: 40px;
+        float: left;
+        margin-top: 10px;
+        margin-left: 40px;
+    }
+
+    #articleHeader .userInfo .loginBtn {
+        width: 100px;
+        height: 34px;
+        border-radius: 6px;
+        background-color: #0066FF;
+        border: 1px solid #0066FF;
+        color: #fff;
+        float: left;
+        margin-top: 4px;
+    }
+
+    #articleHeader .userInfo .loginBtn:hover {
+        cursor: pointer;
+        background-color: #005ce6;
+    }
+
+    #articleHeader .userInfo .userFunction .userImg {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+    }
+
+    #articleHeader .userInfo .userFunction .userImg:hover {
+        cursor: pointer;
+    }
+</style>
